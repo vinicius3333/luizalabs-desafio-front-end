6c012db5c042a5177f1cf36df1b5ed3565e4265b
Vinicius Martins <vinicius-luiz-rodrigues@outlook.com>
2022-01-16 13:31:37 -0300

    add stome e2e tests

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,7 +1,37 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe("My First Test", () => {
+describe("Integration test", () => {
+  const dummyArray = [1, 2, 3];
+
   it("Visits the app root url", () => {
     cy.visit("/");
+  });
+
+  it("Add some products to wishlist", () => {
+    dummyArray.forEach((id) => {
+      cy.get(`:nth-child(${id}) > .card-container-checkbox`).click();
+    });
+  });
+
+  it("Change to wishlist page", () => {
+    cy.get(".header-container-wrapper-items > :nth-child(3)").click();
+    cy.url().should("include", "/wishlist");
+  });
+
+  it("Remove some products from wishlist", () => {
+    dummyArray.forEach((id) => {
+      cy.get(`:nth-child(${id}) > .card-button-close`).click();
+    });
+  });
+
+  it("Check if empty message exists", () => {
+    cy.get("h3").should("be.visible");
+  });
+
+  it("Go back to home by breadcrumb", () => {
+    cy.get(
+      ".wishlist-breadcrumb > :nth-child(1) > .router-link-active"
+    ).click();
+    cy.url().should("include", "/");
   });
 });
